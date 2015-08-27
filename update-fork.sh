371476a76e029f89f156eb7edb2c056dc728996a
Matthew X. Economou <xenophon@irtnog.org>
2015-08-27 09:25:55 -0400

    feat(*fork*): make GitHub fork management scripts sync with all upstream branches
    At the same time, simplify how the scripts are called and written, and
    add comments inline.

--- update-fork.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd $1
-BRANCH=`git branch | grep '^\*' | awk '{print $2}'`
-git fetch upstream
-git merge --ff-only upstream/$BRANCH
-git push
-cd ..
--- update-forks.csh
+++
@@ -1,5 +0,0 @@
-#!/bin/csh
-
-foreach i (`cat forks`)
-  ./update-fork.sh $i
-end
